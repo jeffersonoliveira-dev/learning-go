7effc4500bd832c2d279e8df8f5cfda403ca94c4
jefferson oliveira <jeffersonoliveiradev@gmail.com>
2026-05-13 17:21:12 -0300

    add shorten route and treatment for payload

--- short-url-api/cmd/http/main.go
+++ short-url-api/cmd/http/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	shorten "short-url-api/internal/http/handlers"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -11,9 +12,7 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Post("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
-	})
+	r.Post("/", shorten.Shorten)
 	fmt.Println("listen on port :3000")
 	http.ListenAndServe(":3000", r)
 }
