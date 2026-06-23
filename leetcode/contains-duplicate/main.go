ca140908ab3c7298ca111eb42cf0d28ac6e4caba
jefferson oliveira <jeffersonoliveiradev@gmail.com>
2026-06-23 20:08:49 -0300

    contains duplicate

---
+++ leetcode/contains-duplicate/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func containsDuplicate(nums []int) bool {
+	hash := make(map[int]int)
+	for _, num := range nums {
+		fmt.Println(hash[num])
+
+		if hash[num] > 0 {
+			return true
+		} else {
+			hash[num]++
+		}
+	}
+	return false
+}
+
+func main() {
+
+	//drive
+	nums := []int{1,2,3,}
+
+	fmt.Println(containsDuplicate(nums))
+}
